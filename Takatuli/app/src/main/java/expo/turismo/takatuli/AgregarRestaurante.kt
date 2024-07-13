d2e1e1d9961d6775868f2fab8b0278433e4fbd2d
Bryan Cornejo <20230223@ricaldone.edu.sv>
2024-07-13 14:53:44 -0600

    Scroll View Terminado

--- Takatuli/app/src/main/java/expo/turismo/takatuli/AgregarRestaurante.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/AgregarRestaurante.kt
@@ -31,7 +31,12 @@ class AgregarRestaurante : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar_restaurante, container, false)
+        val root = inflater.inflate(R.layout.fragment_agregar_restaurante, container, false)
+        val txtNombreRestaurante = (R.id.txtNombreRestaurante)
+        return root
+
+
+
     }
 
     companion object {
