7c461dcd04cfac9a6f9c867a1ce4e1309d8f9759
Amaris <amarisosorio10@gmail.com>
2024-07-14 13:05:12 -0600

    Ya sube img a Firebase

--- Takatuli/app/build.gradle.kts
+++ Takatuli/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -53,6 +54,7 @@ dependencies {
     implementation(files("libs\\additionnal.jar"))
     implementation(files("libs\\mail.jar"))
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Takatuli/app/src/main/java/expo/turismo/takatuli/fragment_fotoperfil.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/fragment_fotoperfil.kt
@@ -1,10 +1,13 @@
 package expo.turismo.takatuli
 
+import android.app.Activity.RESULT_OK
+import android.app.ProgressDialog
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Path
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,17 +15,23 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.UUID
 
 
 class fragment_fotoperfil : Fragment() {
 
     lateinit var imageView: ImageView
-    lateinit var miPath: String
+    lateinit var galeria: Button
+    lateinit var subirImagen:Button
+    var fileUri: Uri? = null
+    /*lateinit var miPath: String
 
     val codigoGaleria = 102
     val codigoTomarFoto = 103
     val Camera_request_code = 0
-    val Storage_request_code = 1
+    val Storage_request_code = 1*/
 
     //val uuid = UUID.randomUUID().toString()
 
@@ -43,19 +52,58 @@ class fragment_fotoperfil : Fragment() {
 
         //madar a llamar a todos los elementos
         imageView = root.findViewById(R.id.imgFotoPerfil)
-        val btnGaleria = root.findViewById<Button>(R.id.btnGaleria)
-        val btnTomarFoto = root.findViewById<Button>(R.id.btnTomarFoto)
+        galeria = root.findViewById<Button>(R.id.btnGaleria)
+        subirImagen = root.findViewById<Button>(R.id.btnSubirImg)
 
-        btnGaleria.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent, codigoGaleria)
+
+        galeria.setOnClickListener(){
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_PICK
+            startActivityForResult(Intent.createChooser(intent,"Escoge una imagen"),0)
+        }
+
+        subirImagen.setOnClickListener(){
+            if(fileUri!=null){
+                subirImagen()
+            }else{
+                Toast.makeText(context?.applicationContext,"Seleccione una imagen", Toast.LENGTH_LONG).show()
+            }
         }
 
 
-
-     return root
+        return root
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null && data.data != null){
+            fileUri = data.data
+            try {
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver,fileUri)
+                imageView.setImageBitmap(bitmap)
+            }catch (e: Exception){
+                Log.e("Exception", "Este es el error: " +e)
+            }
+        }
+    }
 
+    fun subirImagen(){
+        if (fileUri!= null){
+            val progressDialog = ProgressDialog(requireContext())
+            progressDialog.setTitle("Upload Image...")
+            progressDialog.setMessage("Procesando...")
+            progressDialog.show()
+
+            val ref: StorageReference = FirebaseStorage.getInstance().getReference().child(UUID.randomUUID().toString())
+            ref.putFile(fileUri!!).addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(context?.applicationContext, "El archivo se subio correctamente", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                progressDialog.dismiss()
+                Toast.makeText(context?.applicationContext, "Ha ocurrido un error en la descarga...",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
 }
