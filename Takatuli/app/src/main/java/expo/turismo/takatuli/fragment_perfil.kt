f5d7af56a3c234e3b8b23d7452e97f4b22e4c3d8
Amaris <amarisosorio10@gmail.com>
2024-07-14 20:30:13 -0600

    conflicto en la master

--- Takatuli/app/src/main/java/expo/turismo/takatuli/fragment_perfil.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/fragment_perfil.kt
@@ -1,12 +1,8 @@
 package expo.turismo.takatuli
 
-<<<<<<< HEAD
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-=======
-import android.content.Intent
->>>>>>> a86306b049036ba3215683cb52b860ec8d11afd6
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
@@ -14,19 +10,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-<<<<<<< HEAD
 import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import expo.turismo.takatuli.databinding.FragmentPerfilBinding
 import java.nio.file.Path
 import java.util.UUID
-=======
+
 import androidx.navigation.fragment.findNavController
->>>>>>> a86306b049036ba3215683cb52b860ec8d11afd6
 
 class fragment_perfil : Fragment() {
-<<<<<<< HEAD
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,16 +37,14 @@ class fragment_perfil : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
 
 
     }
     private var _binding:FragmentPerfilBinding? = null
     private val binding get() = _binding!!
-=======
->>>>>>> a86306b049036ba3215683cb52b860ec8d11afd6
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
