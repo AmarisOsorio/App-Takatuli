ec9002e5f37d7cbbf95e7932c0dd9fb1deed0a5e
unknown <20230154@ricaldone.edu.sv>
2024-08-22 12:08:46 -0600

    Arreglo de el merge

--- Takatuli/app/src/main/java/expo/turismo/takatuli/Modelo/ClaseConexion.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/Modelo/ClaseConexion.kt
@@ -8,16 +8,12 @@ class ClaseConexion {
 
         try {
 
-<<<<<<< HEAD
             val url = "jdbc:oracle:thin:@172.19.176.1:xe"
             val usuario = "TakatuliPTC"
             val contrasena = "Takatuli2A"
-=======
 
-            val url = "jdbc:oracle:thin:@192.168.0.5:1521:xe"
-            val usuario = "tbRolUs"
-            val contrasena = "1234"
->>>>>>> master
+
+
 
             val connection = DriverManager.getConnection(url,usuario,contrasena)
             return connection
