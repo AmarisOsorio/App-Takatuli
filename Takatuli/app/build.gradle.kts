087926c8dd468d9fa47a6a8b51db400c9290485c
Bryan Cornejo <20230223@ricaldone.edu.sv>
2024-09-19 11:43:21 -0600

    Listo diseños tambien terminada las cards

--- Takatuli/app/build.gradle.kts
+++ Takatuli/app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- Takatuli/app/src/main/java/expo/turismo/takatuli/RecyclerViewMostrar/ViewHolder.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/RecyclerViewMostrar/ViewHolder.kt
@@ -1,10 +1,12 @@
 package expo.turismo.takatuli.RecyclerViewMostrar
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import expo.turismo.takatuli.R
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val txtNombreLugarTuristico: TextView = view.findViewById(R.id.txtNombreHost)
+    var ImgLugarTuristico: ImageView = view.findViewById(R.id.imgLugarTuristico)
 }
