c90eca1cbfdee77c163f38714bc7b0ab9a04990e
Bryan Cornejo <20230223@ricaldone.edu.sv>
2024-09-24 21:48:02 -0600

    Llevo a medias el trabajo de pasar campos a otra pantalla (problema de imagen)

---
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/activity_detallesRestaurante.kt
@@ -0,0 +1,20 @@
+package expo.turismo.takatuli
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_detallesRestaurante : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalles_restaurante)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
