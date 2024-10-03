05cf40ac6e08139b97937e11dae0a896c450eedf
Bryan Cornejo <20230223@ricaldone.edu.sv>
2024-10-03 10:31:36 -0600

    Diseñitos

--- Takatuli/app/src/main/java/expo/turismo/takatuli/Acerca_de.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/Acerca_de.kt
@@ -3,6 +3,7 @@ package expo.turismo.takatuli
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,13 @@ class Acerca_de : AppCompatActivity() {
         Aceptarb.setOnClickListener {
             val intent= Intent(this,fragment_ajustes::class.java)
             startActivity(intent)
+
+            val btnatrasT = findViewById<ImageView>(R.id.imgatrasacercade)
+
+            btnatrasT.setOnClickListener {
+                val iratrasacercade = Intent(this, MainActivity::class.java)
+                startActivity(iratrasacercade)
+            }
         }
 
 
--- Takatuli/app/src/main/java/expo/turismo/takatuli/Politicas_de_Privacidad.kt
+++ Takatuli/app/src/main/java/expo/turismo/takatuli/Politicas_de_Privacidad.kt
@@ -3,6 +3,7 @@ package expo.turismo.takatuli
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,12 @@ class Politicas_de_Privacidad : AppCompatActivity() {
         Aceptar.setOnClickListener {
             val intent= Intent(this,fragment_ajustes::class.java)
             startActivity(intent)
+        }
+        val btnatrasP = findViewById<ImageView>(R.id.imgatraspoliticaprivacida)
+
+        btnatrasP.setOnClickListener {
+            val iratraspoliticaprivacida = Intent(this, MainActivity::class.java)
+            startActivity(iratraspoliticaprivacida)
         }
     }
 
